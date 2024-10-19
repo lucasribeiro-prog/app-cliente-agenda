53b2a472f659d3c8c148a0831db581511ba8d6ff
Lucas Ribeiro <lucasribeiro1999.ll@gmail.com>
2024-10-18 23:33:38 -0300

    Altendo o tamanho padrão das colunas cfp da tabela clientes e telefone da tabela contatos e criptografando esses dados no banco

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Cliente extends Model
 {
     protected $fillable = ['nome', 'cpf', 'matricula'];
+
+    //Criptografar o CPF antes de salvar no banco
+    public function setCpfAttribute($value)
+    {
+    $this->attributes['cpf'] = Crypt::encryptString($value);
+    }
+
+    //Descriptografar o CPF ao recuperar do banco
+    public function getCpfAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
 }
--- app/Models/Contato.php
+++ app/Models/Contato.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Contato extends Model
 {
     protected $fillable = ['id_cliente', 'telefone'];
+
+    // Mutator para criptografar o telefone antes de salvar no banco
+    public function setTelefoneAttribute($value)
+    {
+        $this->attributes['telefone'] = Crypt::encryptString($value);
+    }
+
+    // Accessor para descriptografar o telefone ao recuperar do banco
+    public function getTelefoneAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
 }
