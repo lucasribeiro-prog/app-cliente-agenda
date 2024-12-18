18cdcdd36f0524a7b5f774e9169b6d14b9fd41a7
Lucas Ribeiro <lucasribeiro1999.ll@gmail.com>
2024-12-18 20:54:39 -0300

    Adicionando arquivos de build ao repositório

---
+++ public/build/assets/ApplicationLogo-D8y4u356.js
@@ -0,0 +1,1 @@
+import{m as o,o as c,f as a}from"./app-De219XSa.js";const e={},s={class:"fa-solid fa-calendar-days fa-3x ml-7"};function n(t,r){return c(),a("i",s)}const f=o(e,[["render",n]]);export{f as A};
